b71a09d70b89c442372df1d93b60f835914e637b
Brian Friesen <friesen@ou.edu>
2012-11-12 16:14:05 -0600

    Using GSL physical constants instead of hard-coding them.

--- src/const.h
+++ src/const.h
@@ -1,11 +1,13 @@
 #ifndef CONST_H
 #define CONST_H
 
+#include<gsl/gsl_const_cgsm.h>
+
 /** Namespace with physical constants. Units are CGS unless explicitly stated
  *  otherwise. */
 namespace physconst
 {
-    const double c_light = 2.99792458e10; /** Speed of light. */
+    const double c_light = GSL_CONST_CGSM_SPEED_OF_LIGHT; /** Speed of light. */
     const double cm2km = 1.0e-5; /** Convert cm to km. */
 }
 
