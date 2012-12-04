8f80786e15b52e806270d64b87083dfded68f67c
Brian Friesen <friesen@ou.edu>
2012-12-04 11:20:48 -0600

    Fixed indentation on initialization statements for exception class constructors.

--- src/my_exceptions.cpp
+++ src/my_exceptions.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 Exception::Exception(const string& msg_)
-: runtime_error(build_what(msg_)),
-  msg(msg_)
+    : runtime_error(build_what(msg_)),
+      msg(msg_)
 {}
 
 Exception::~Exception() throw()
@@ -23,25 +23,25 @@ FileNotFound::FileNotFound(string filename)
 {}
 
 InterpOutOfRange::InterpOutOfRange(double x)
-: Exception(ErrorMsg::INTERP_OUT_OF_RANGE + boost::lexical_cast<string>(x))
+    : Exception(ErrorMsg::INTERP_OUT_OF_RANGE + boost::lexical_cast<string>(x))
 {}
 
 WrongCLIUsage::WrongCLIUsage()
-: Exception(ErrorMsg::WRONG_CLI_USAGE)
+    : Exception(ErrorMsg::WRONG_CLI_USAGE)
 {}
 
 NonmonotonicVelocityField::NonmonotonicVelocityField(double vel)
-: Exception(ErrorMsg::NONMONOTONIC + boost::lexical_cast<string>(vel))
+    : Exception(ErrorMsg::NONMONOTONIC + boost::lexical_cast<string>(vel))
 {}
 
 NegativeVelocity::NegativeVelocity(double vel)
-: Exception(ErrorMsg::NEGATIVE_VELOCITY + boost::lexical_cast<string>(vel))
+    : Exception(ErrorMsg::NEGATIVE_VELOCITY + boost::lexical_cast<string>(vel))
 {}
 
 SuperluminalVelocity::SuperluminalVelocity(double vel)
-: Exception(ErrorMsg::SUPERLUMINAL_VELOCITY + boost::lexical_cast<string>(vel))
+    : Exception(ErrorMsg::SUPERLUMINAL_VELOCITY + boost::lexical_cast<string>(vel))
 {}
 
 InvalidElement::InvalidElement(int Z, int I)
-: Exception(ErrorMsg::INVALID_ELEMENT + "(" + boost::lexical_cast<string>(Z) + ", " + boost::lexical_cast<string>(I) + ")")
+    : Exception(ErrorMsg::INVALID_ELEMENT + "(" + boost::lexical_cast<string>(Z) + ", " + boost::lexical_cast<string>(I) + ")")
 {}
