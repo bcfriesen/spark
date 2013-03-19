60be4244ae18c82325a8afc9aa043eef59d43e34
Brian Friesen <friesen@ou.edu>
2013-03-19 16:40:02 -0500

    Updated to new yaml-cpp API.
    The new API was introduced in version 0.5.0. Since it is very different
    (and in my opinion very much better) than the old API, the upgrade in
    this code was worth doing.

--- src/read_params.cpp
+++ src/read_params.cpp
@@ -8,30 +8,20 @@
 using namespace std;
 void read_params(string yaml_file, ParamsClass* params)
 {
-    /* YAML file has all the file locations, parameters, etc. */
-    ifstream infile(yaml_file.c_str(), ifstream::in);
-
-    if (!infile) throw FileNotFound(yaml_file);
-
-    // I stole this try...catch segment straight from the YAML tutorial.
     try
     {
-        YAML::Parser parser(infile);
-        YAML::Node doc;
-        parser.GetNextDocument(doc);
+        YAML::Node doc(yaml_file.c_str());
         // we read PHOENIX-style layer files
-        doc["layer_file"] >> params->layer_file;
+        params->layer_file = doc["layer_file"].as<std::string>();
         // read # of core-intersecting rays (this is a knob)
-        doc["num_core_intersect_rays"] >> params->num_core_intersect_rays;
+        params->num_core_intersect_rays = doc["num_core_intersect_rays"].as<unsigned int>();
         // get name of file for dumping output messages
-        doc["output_file"] >> params->output_file;
+        params->output_file = doc["output_file"].as<std::string>();
     }
     catch(YAML::ParserException& e)
     {
         cout << e.what() << endl;
     }
-
-    infile.close();
 
     return;
 }
